8c00fde8961458b0452684176d7008076d12a77a
Icaro Roberto <icaro.santos@pravaler.com.br>
2020-04-27 18:28:34 -0300

    Volume Correction

--- jenkins-master/config/config.sh
+++ jenkins-master/config/config.sh
@@ -1,5 +1,9 @@
 sleep 30
 
+java -jar /jenkins_config/jenkins-cli.jar -s http://localhost:8080/ groovy /jenkins_config/convert_key.groovy > encrypted_key
+export encrypted_key=$(cat encrypted_key)
+sed -i 's/$PRIVATE_KEY/'$encrypted_key'/' /jenkins_config/jenkins.yaml
+
 java -jar /jenkins_config/jenkins-cli.jar -s http://localhost:8080/ apply-configuration < /jenkins_config/jenkins.yaml
 java -jar /jenkins_config/jenkins-cli.jar -s http://localhost:8080/ create-job __jobs-pipeline__ < /jenkins_config/jobs-pipeline.xml
 ssh -T git@bitbucket.org
