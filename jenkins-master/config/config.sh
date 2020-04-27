a373e4125fe7bcdcb7e115717208ccf941362d33
Icaro Roberto <icaro.santos@pravaler.com.br>
2020-04-27 14:59:09 -0300

    Volume Correction

--- jenkins-master/config/config.sh
+++ jenkins-master/config/config.sh
@@ -1,4 +1,4 @@
-sleep 15
+sleep 30
 java -jar /jenkins_config/jenkins-cli.jar -s http://localhost:8080/ apply-configuration < /jenkins_config/jenkins.yaml
 java -jar /jenkins_config/jenkins-cli.jar -s http://localhost:8080/ create-job __jobs-pipeline__ < /jenkins_config/jobs-pipeline.xml
 
